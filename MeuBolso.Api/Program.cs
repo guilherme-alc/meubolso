17e4242223f4538eaa2e21aeea87d610e2a79d0c
guilherme-alc <guilhermealc01@gmail.com>
2025-02-21 00:55:49 -0300

    ✨ feat: adiciona Swagger para documentação da API

--- MeuBolso.Api/Program.cs
+++ MeuBolso.Api/Program.cs
@@ -1,5 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+// Adiciona suporte para Open Api
+builder.Services.AddEndpointsApiExplorer();
+// Adiciona suporte a interface do Swagger
+builder.Services.AddSwaggerGen(opts =>
+{
+    // Full Qualified Name
+    opts.CustomSchemaIds(type => type.FullName);
+});
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
