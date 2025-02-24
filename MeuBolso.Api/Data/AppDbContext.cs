92476ca64a0b0636496ea023670ccd5953595028
guilherme-alc <guilhermealc01@gmail.com>
2025-02-24 20:07:56 -0300

    ✨ feat: adiciona  migrations e Fluent API para as entidades

--- MeuBolso.Api/Data/AppDbContext.cs
+++ MeuBolso.Api/Data/AppDbContext.cs
@@ -1,15 +1,23 @@
-﻿using MeuBolso.Core.Models;
+﻿using MeuBolso.Api.Data.Mappings;
+using MeuBolso.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MeuBolso.Api.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+            
+        }
         public DbSet<Category> Categories { get; set; } = null!;
         protected DbSet<Transaction> Transactions { get; set; } = null!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoryMapping());
+            modelBuilder.ApplyConfiguration(new TransactionMapping());
+
         }
     }
 }
