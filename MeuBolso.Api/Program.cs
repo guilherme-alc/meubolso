78b9929635b6614b8163a8f7778f2f45f90a4cbe
guilherme-alc <guilhermealc01@gmail.com>
2025-03-19 07:00:16 -0300

    chore: adiciona pacote identity para autenticação

--- MeuBolso.Api/Program.cs
+++ MeuBolso.Api/Program.cs
@@ -2,9 +2,23 @@ using MeuBolso.Api.Data;
 using MeuBolso.Api.Endpoints;
 using MeuBolso.Api.Handlers;
 using MeuBolso.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona suporte para Open Api
+builder.Services.AddEndpointsApiExplorer();
+// Adiciona suporte a interface do Swagger
+builder.Services.AddSwaggerGen(opts =>
+{
+    // Full Qualified Name
+    opts.CustomSchemaIds(type => type.FullName);
+});
+
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
 
 var connectionString = builder
     .Configuration
@@ -19,15 +33,6 @@ builder.Services.AddDbContext<AppDbContext>(
 
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
-
-// Adiciona suporte para Open Api
-builder.Services.AddEndpointsApiExplorer();
-// Adiciona suporte a interface do Swagger
-builder.Services.AddSwaggerGen(opts =>
-{
-    // Full Qualified Name
-    opts.CustomSchemaIds(type => type.FullName);
-});
 
 var app = builder.Build();
 app.UseSwagger();
